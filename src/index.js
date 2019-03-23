3486dc073e7c874aa80d26cf2319f084b0fc28a2
Igor Semak <6399815@mail.ru>
2019-03-23 18:24:28 +0300

    docs:add Homework advans

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
-}
+    
+  var sem = Number.MAX_VALUE;
+  var ak = base;
+  for (var q = 2; q <= base; q++) {
+    if (ak % q === 0) {        
+      var lis = 0;
+        
+      while (ak % q === 0) 
+          
+      {
+         ak = ak / q;     
+         lis++;
+      }
+        
+      var x = 0;  
+      var z = Math.floor(number / q);
+        
+      while (z > 0) 
+      {
+        x += z;
+        z = Math.floor(z / q);
+      }
+      sem = Math.min(sem, Math.floor(x / lis))
+    }
+  }
+            return sem;
+        }
